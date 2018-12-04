28b557af8c0c981401674aa04221622439344217
Michele De La Pena <mdelapena@stsci.edu>
2018-12-04 15:33:02 -0500

    Updated filter routine

--- hlapipeline/utils/filter.py
+++ hlapipeline/utils/filter.py
@@ -1,9 +1,10 @@
 """ Utility to filter datasets which cannot be aligned 
 
-The function analyze_data opens the input FITS file to access the primary 
-header data.  Based upon the values of certain FITS keywords, the function
-determines whether or not this dataset can be reconciled against an astrometric 
-catalog and, for multiple images, used to create a mosaic.
+The function analyze_data opens an input list containing FLT or FLC FITS file 
+names in order to access the primary header data.  Based upon the values of specified 
+FITS keywords, the function determines whether or not each file within this dataset 
+can be reconciled against an astrometric catalog and, for multiple images, used to create 
+a mosaic.
 
 """
 from astropy.io import fits
@@ -12,19 +13,23 @@ import math
 
 __all__ = ['analyze_data']
 
-def analyze_data(inputFileName):
+#def analyze_data(inputFileList, optionalCols = None):
+def analyze_data(inputFileList, **kwargs):
     """
-    Determine if the dataset can be aligned
+    Determine if images within the dataset can be aligned
    
     Parameters
     ==========
-    inputFileName: str
-        Input FLT or FLC filename
+    inputFileList: list
+        List containing FLT or FLC filenames for all input images which comprise an associated 
+        dataset where 'associated dataset' may be a single image, multiple images, an association, 
+        or a number of associations
 
     Returns
     =======
     outputTable: object
-        Astropy Table object containing data pertaining to the dataset, including the doProcess bool
+        Astropy Table object containing data pertaining to the associated dataset, including 
+        the doProcess bool
 
     The keyword/value pairs below define the "cannot process categories".
     OBSTYPE: not IMAGING
@@ -42,85 +47,102 @@ def analyze_data(inputFileName):
     APKEY  = 'APERTURE'
     TARKEY = 'TARGNAME'
     EXPKEY = 'EXPTIME'
+    DATEKEY = 'DATE-OBS'
 
-    header_hdu  = 0
-    header_data = fits.getheader(inputFileName, header_hdu)
+    # Create an astropy table
+    #optionalCols = kwargs.get('optionalCols', None)
 
-    obstype  = (header_data[OBSKEY]).upper()
-    mtflag   = (header_data[MTKEY]).upper()
-    scan_typ = (header_data[SCNKEY]).upper()
-
-    sfilter  = (header_data[FILKEY]).upper()
-    aperture = (header_data[APKEY]).upper()
-    targname = (header_data[TARKEY]).upper()
-    expotime = header_data[EXPKEY]
-
-    # Keywords to use potentially for analysis
-    instrume = (header_data['INSTRUME']).upper()
-    detector = (header_data['DETECTOR']).upper()
-    subarray = header_data['SUBARRAY']
-
-    # Determine if the image has one of these conditions.  The routine
-    # will exit processing upon the first satisfied condition.
-
-    noProcKey   = None
-    noProcValue = None
-    doProcess = True
-    # Imaging vs spectroscopic or coronagraphic
-    if obstype != 'IMAGING':
-        noProcKey   = OBSKEY
-        noProcValue = obstype 
-
-    # Moving target
-    elif mtflag == 'T':
-        noProcKey   = MTKEY
-        noProcValue = mtflag 
-
-    # Bostrophidon without or with dwell
-    elif any ([scan_typ == 'C', scan_typ == 'D']):
-        noProcKey   = SCNKEY
-        noProcValue = scan_typ
-
-    # Filter which begins with !F (e.g., 'POL*' or 'G*')
-    elif sfilter[0] != 'F': 
-        noProcKey   = FILKEY
-        noProcValue = sfilter
-
-    # Ramp, polarizer, or grism 
-    elif any (['RAMP' in aperture, 'POL' in aperture, 'GRISM' in aperture, 
-              '-REF' in aperture]):
-        noProcKey   = APKEY
-        noProcValue = aperture 
-
-    # Calibration target
-    elif any (['DARK' in targname, 'TUNG' in targname, 'BIAS' in targname, 
-              'FLAT' in targname, 'DEUT' in targname, 'EARTH-CAL' in targname]):
-        noProcKey   = TARKEY
-        noProcValue = targname
-
-    # Exposure time of zero
-    elif math.isclose(expotime, 0.0, abs_tol=1e-5)
-        noProcKey   = EXPKEY
-        noProcValue = expotime 
-
-    if (noProcKey is not None):
-        doProcess = False
-
-        # Issue message to log file 
-        issue_msg(inputFileName, noProcKey, noProcValue)
-
-    # Create an astropy table and start to populate it 
     catalog = None
-    foundSources = None
+    foundSources = 0 
     rms_x = None
     rms_y = None
-    matchSources = None
+    matchSources = 0 
     isSuccess = False
-    namesArray=('instrument', 'detector', 'filter', 'aperture', 'obstype', 
-            'subarray', 'doProcess', 'catalog', '# found sources', 'rms_x', 
-            'rms_y', '# match sources', 'isSuccess')
-    outputTable = (namesArray, [instrume, detector, sfilter, aperture, obstype, subarray, doProcess, catalog, 
-                   foundSources, rms_x, rms_y, matchSources, isSuccess])
+    #isSuccess = 0
+    dateObs = None
+    namesArray = ('imageName', 'instrument', 'detector', 'filter', 'aperture', 'obstype', 
+            'subarray', 'dateObs', 'doProcess', 'catalog', '# found sources', '# match sources', 
+            'rms_x', 'rms_y', 'isSuccess')
+    #dataType = ('S20', 'S20', 'S20', 'S20', 'S20', 'S20', 'i1', 'S20', 'i1', 'S20', 'i4', 'i4', 'f8', 'f8', 'i1')
+    dataType = ('S20', 'S20', 'S20', 'S20', 'S20', 'S20', 'b', 'S20', 'b', 'S20', 'i4', 'i4', 'f8', 'f8', 'b')
+
+    outputTable = Table(names=namesArray,dtype=dataType)
+
+    # Loop over the list of images to determine viability for alignment processing
+    for inputFile in inputFileList:
+
+        header_hdu  = 0
+        header_data = fits.getheader(inputFile, header_hdu)
+
+        obstype  = (header_data[OBSKEY]).upper()
+        mtflag   = (header_data[MTKEY]).upper()
+        scan_typ = (header_data[SCNKEY]).upper()
+
+        sfilter  = (header_data[FILKEY]).upper()
+        aperture = (header_data[APKEY]).upper()
+        targname = (header_data[TARKEY]).upper()
+        expotime = header_data[EXPKEY]
+
+        # Keywords to use potentially for analysis
+        instrume = (header_data['INSTRUME']).upper()
+        detector = (header_data['DETECTOR']).upper()
+        subarray = header_data['SUBARRAY']
+        dataObs  = header_data['DATE-OBS']
+
+        # Determine if the image has one of these conditions.  The routine
+        # will exit processing upon the first satisfied condition.
+
+        noProcKey   = None
+        noProcValue = None
+        doProcess = True
+        # Imaging vs spectroscopic or coronagraphic
+        if obstype != 'IMAGING':
+            noProcKey   = OBSKEY
+            noProcValue = obstype 
+
+        # Moving target
+        elif mtflag == 'T':
+            noProcKey   = MTKEY
+            noProcValue = mtflag 
+
+        # Bostrophidon without or with dwell
+        elif any ([scan_typ == 'C', scan_typ == 'D']):
+            noProcKey   = SCNKEY
+            noProcValue = scan_typ
+
+        # Filter which begins with !F (e.g., 'POL*' or 'G*')
+        elif sfilter[0] != 'F': 
+            noProcKey   = FILKEY
+            noProcValue = sfilter
+
+        # Ramp, polarizer, or grism 
+        elif any (['RAMP' in aperture, 'POL' in aperture, 'GRISM' in aperture, 
+                  '-REF' in aperture]):
+            noProcKey   = APKEY
+            noProcValue = aperture 
+
+        # Calibration target
+        elif any (['DARK' in targname, 'TUNG' in targname, 'BIAS' in targname, 
+                  'FLAT' in targname, 'DEUT' in targname, 'EARTH-CAL' in targname]):
+            noProcKey   = TARKEY
+            noProcValue = targname
+
+        # Exposure time of zero
+        elif math.isclose(expotime, 0.0, abs_tol=1e-5):
+            noProcKey   = EXPKEY
+            noProcValue = expotime 
+
+        if (noProcKey is not None):
+            #doProcess = 0
+            doProcess = False
+
+        # Issue message to log file 
+        #issue_msg(inputFileList, noProcKey, noProcValue)
+
+        # Populate a row of the table
+        outputTable.add_row([inputFile, instrume, detector, sfilter, aperture, obstype, 
+                             subarray, dateObs, doProcess, catalog, foundSources, matchSources, 
+                             rms_x, rms_y, isSuccess])
 
     return(outputTable)
 
